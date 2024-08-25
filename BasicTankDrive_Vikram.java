57f11e3046403f5e7e80e52608cbc7bb4c298de1
VJ <114836259+vik-j@users.noreply.github.com>
2024-08-25 16:29:04 -0700

    Add files via upload

--- BasicTankDrive_Vikram.java
+++ BasicTankDrive_Vikram.java
@@ -2,20 +2,43 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import static org.firstinspires.ftc.teamcode.BasicTankDrive_Vikram.TurnDirection.*;
 
 public class BasicTankDrive_Vikram extends LinearOpMode {
-    public DcMotor left,right;
+    public DcMotor fl, fr, bl, br;
+
+    public boolean GoodSteering = false;
+    public enum TurnDirection {
+        LEFT,RIGHT
+    }
+
+    public void setLeftMotors(double power) {
+        fl.setPower(power);
+        bl.setPower(power);
+    }
+    public void setRightMotors(double power) {
+        fr.setPower(power);
+        br.setPower(power);
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
-        left = hardwareMap.get(DcMotor.class, "left");
-        right = hardwareMap.get(DcMotor.class, "right");
+        fl = hardwareMap.get(DcMotor.class, "Front Left");
+        fr = hardwareMap.get(DcMotor.class, "Front Right");
+        bl = hardwareMap.get(DcMotor.class, "Back Left");
+        br = hardwareMap.get(DcMotor.class, "Back Right");
+
+        TurnDirection direction = null;
 
         waitForStart();
 
         while (opModeIsActive() && !isStopRequested()) {
-            left.setPower(-gamepad1.left_stick_x);
-            right.setPower(-gamepad1.right_stick_x);
+            double leftY = -gamepad1.left_stick_y;
+            double rightY = -gamepad1.right_stick_y;
+
+            setLeftMotors(leftY);
+            setRightMotors(rightY);
+
         }
     }
 }
